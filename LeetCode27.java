405d715d41b0f0cf902e0406e1165ea164ddeb92
unknown <trongpvn2211@gmail.com>
2025-07-07 10:37:54 +0700

    Remove Element

---
+++ LeetCode27.java
@@ -0,0 +1,18 @@
+public class LeetCode27 {
+    public static void main(String[] args) {
+        System.out.println(removeElement(new int[]{3,2,2,3}, 2));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        for (int i=0; i<n; ){
+            if (nums[i]==val){
+                for (int j=i; j<=n-2; j++){
+                    nums[j] = nums[j+1];
+                }
+                n--;
+            } else i++;
+        }
+        return n;
+    }
+}
