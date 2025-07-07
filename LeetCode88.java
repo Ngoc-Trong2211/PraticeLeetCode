14cec2e4455ba0afb2378b86e08ee752ab666884
Ngoc-Trong2211 <146172045+Ngoc-Trong2211@users.noreply.github.com>
2025-07-07 18:46:05 +0700

    use 2 pointer

--- LeetCode88.java
+++ LeetCode88.java
@@ -1,32 +1,56 @@
 public class LeetCode88 {
     public static void main(String[] args) {
-        int a[] = {2,0};
-        int b[] = {1};
-        merge(a, 1, b, 1);
+        int a[] = {1,2,0,0};
+        int b[] = {1,5};
+        merge(a, 2, b, 2);
         for (int i : a){
             System.out.print(i + " ");
         }
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int ai : nums2){
-            addElementIntoNums1(ai, nums1, m);
-            m++;
+        // 2 pointer
+        int k = m+n-1;
+        int i = m-1;
+        int j = n-1;
+        while (k>=0){
+            if (j<0){
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else if (i<0){
+                nums1[k] = nums2[j];
+                j--;
+            }
+            else if (nums1[i]>nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else if (nums1[i]<=nums2[j]){
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
         }
+
+        // for (int ai : nums2){
+        //     addElementIntoNums1(ai, nums1, m);
+        //     m++;
+        // }
     }
 
-    private static void addElementIntoNums1(int ai, int[] nums1, int m) {
-        boolean isChange = false;
-        for (int i=0; i<nums1.length; i++){
-            if (nums1[i]>ai){
-                isChange = true;
-                for (int j=m-1; j>=i; j--){
-                    nums1[j+1] = nums1[j];
-                }
-                nums1[i] = ai;
-                break;
-            }
-        }
-        if (!isChange) nums1[m] = ai;
-    }
+    // private static void addElementIntoNums1(int ai, int[] nums1, int m) {
+    //     boolean isChange = false;
+    //     for (int i=0; i<nums1.length; i++){
+    //         if (nums1[i]>ai){
+    //             isChange = true;
+    //             for (int j=m-1; j>=i; j--){
+    //                 nums1[j+1] = nums1[j];
+    //             }
+    //             nums1[i] = ai;
+    //             break;
+    //         }
+    //     }
+    //     if (!isChange) nums1[m] = ai;
+    // }
 }
