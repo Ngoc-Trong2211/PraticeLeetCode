577570de70d4ce021f7983a1fe0bebb818510901
Ngoc-Trong2211 <146172045+Ngoc-Trong2211@users.noreply.github.com>
2025-07-08 18:40:37 +0700

    Richest Customer Wealth

---
+++ LeetCode1672.java
@@ -0,0 +1,21 @@
+// Richest Customer Wealth
+
+public class LeetCode1672 {
+    public static void main(String[] args) {
+        int a[][] = {{1,2,3},{3,2,1}};
+        System.out.println(maximumWealth(a));
+    }
+
+    public static int maximumWealth(int[][] accounts) {
+        int sum = 0;
+        int max = 0;
+        for (int i=0; i<accounts.length; i++){
+            sum = 0;
+            for (int j=0; j<accounts[i].length; j++){
+                sum = sum + accounts[i][j];
+            }
+            if (sum > max) max = sum;
+        }
+        return max;
+    }
+}
