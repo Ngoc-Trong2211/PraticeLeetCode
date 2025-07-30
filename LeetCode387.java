c424d2a12afb8a2b73e8fc6c54c8cc43bee670d6
Ngoc-Trong2211 <146172045+Ngoc-Trong2211@users.noreply.github.com>
2025-07-30 23:03:02 +0700

    First Unique Character in a String

---
+++ LeetCode387.java
@@ -0,0 +1,18 @@
+// First Unique Character in a String
+
+public class LeetCode387 {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int firstUniqChar(String s) {
+        int[] a = new int[26];
+        for (int i=0; i<s.length(); i++){
+            a[s.charAt(i) - 'a']++;
+        }
+        for (int i=0; i<s.length(); i++){
+            if (a[s.charAt(i) - 'a']==1) return i;
+        }
+        return -1;
+    }
+}
