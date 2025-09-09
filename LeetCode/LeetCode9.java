ae2f49aee1568f7a1b35f7d058210eea78f87848
Ngoc-Trong2211 <146172045+Ngoc-Trong2211@users.noreply.github.com>
2025-09-09 15:29:45 +0700

    Palindrome Number

---
+++ LeetCode/LeetCode9.java
@@ -0,0 +1,21 @@
+package LeetCode;
+// Palindrome Number
+
+public class LeetCode9 {
+    public static void main(String[] args) {
+        int b = -121;
+        System.out.println(isPalindrome(b));
+    }
+
+    public static boolean isPalindrome(int x) {
+        int a = 0;
+        int b = x;
+        if (x<0) return false;
+        while (x!=0) {
+            a = a*10 + x%10;
+            x/=10;
+        }
+        if (a==b) return true;
+        return false;
+    }
+}
