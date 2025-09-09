cd8c2e38589facf2505bcd05a8ca753c7471a35b
Ngoc-Trong2211 <146172045+Ngoc-Trong2211@users.noreply.github.com>
2025-09-09 23:48:49 +0700

    Roman to Integer

---
+++ LeetCode/LeetCode13.java
@@ -0,0 +1,49 @@
+package LeetCode;
+// Roman to Integer
+
+public class LeetCode13 {
+    public static void main(String[] args) {
+        String s = "III";
+        System.out.println(romanToInt(s));
+    }
+
+    public static int romanToInt(String s) {
+        int sum = 0;
+        for(int i=0; i<s.length(); i++){
+            int val = convertInteger(s.charAt(i));
+            if (i < s.length() - 1 && val < convertInteger(s.charAt(i+1))){
+                sum -= val;
+            } else sum += val;
+        }
+        return sum;
+    }
+
+    public static int convertInteger(char c){
+        switch (c) {
+            case 'I' -> {
+                return 1;
+            }
+            case 'V' -> {
+                return 5;
+            }
+            case 'X' -> {
+                return 10;
+            }
+            case 'L' -> {
+                return 50;
+            }
+            case 'C' -> {
+                return 100;
+            }
+            case 'D' -> {
+                return 500;
+            }
+            case 'M' -> {
+                return 1000;
+            }
+            default ->{
+                return 0;
+            }
+        }
+    }
+}
