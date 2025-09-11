1c1c6f92c343dc08dff4af2184cfcc10fd1d65a3
HPDuong <trongpvn2211@gmail.com>
2025-09-11 19:36:31 +0700

    Length of Last Word

---
+++ LeetCode/Easy/LeetCode58.java
@@ -0,0 +1,18 @@
+// Length of Last Word
+
+public class LeetCode58 {
+    public static void main(String[] args){
+        String s = "           h asdfasdfas fasdfasd";
+        System.out.println(lengthOfLastWord(s));
+    }
+
+    public static int lengthOfLastWord(String s) {
+        String a = s.trim();
+        int k = 0;
+        for(int i=a.length()-1; i>=0; i--){
+            if (a.charAt(i) != ' ') k++;
+            else break;
+        }
+        return k;
+    }
+}
